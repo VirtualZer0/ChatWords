523d3fa6a4a17ffc722f64a1d1cd1de5f617116a
VirtualZer0 <virtualos@list.ru>
2021-07-29 20:10:15 +0300

    Add vue-i18n

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'ru',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
+}
