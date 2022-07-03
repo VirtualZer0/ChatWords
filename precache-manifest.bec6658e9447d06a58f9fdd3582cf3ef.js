9e4a8060639cd224b27262a2d8a42f1edb0f36e4
VirtualZer0 <45962846+VirtualZer0@users.noreply.github.com>
2022-07-03 16:07:45 +0000

    gh-pages

---
+++ precache-manifest.bec6658e9447d06a58f9fdd3582cf3ef.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772c7c69fed5a9f4b877",
+    "url": "/css/app.22a1c1dd.css"
+  },
+  {
+    "revision": "0fc6657250b99b446f28",
+    "url": "/css/chunk-vendors.00cea7b4.css"
+  },
+  {
+    "revision": "9a09fa0d3071a1261a43",
+    "url": "/css/game.02b3234f.css"
+  },
+  {
+    "revision": "99ed0a78a42578f8d3d8",
+    "url": "/css/gameover.d74e3fb8.css"
+  },
+  {
+    "revision": "31ad7c8d90c81cdfb2e9",
+    "url": "/css/nextlevel.3db9fa7d.css"
+  },
+  {
+    "revision": "fb646d49e37c6991c662035f8930d092",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-300.woff"
+  },
+  {
+    "revision": "b512d2aa094845e60cbf1053583433d3",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-300.woff2"
+  },
+  {
+    "revision": "a7bcd1231e00466c77879d9b007f0c02",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-500.woff"
+  },
+  {
+    "revision": "2b11daa0085dfdee49e43bb430bafea2",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-500.woff2"
+  },
+  {
+    "revision": "08ee47f609d9381a5d39b5d137afc37a",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-700.woff"
+  },
+  {
+    "revision": "89b52491bd99b78a9e4d98ecc656ceba",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-700.woff2"
+  },
+  {
+    "revision": "c02213958a9ca3171f031156286a6112",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-800.woff"
+  },
+  {
+    "revision": "7cac4034339ad6ac87e7133a35d603d8",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-800.woff2"
+  },
+  {
+    "revision": "7bb3065acf6b70a3789e9ff3b045f61c",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-900.woff"
+  },
+  {
+    "revision": "09d8067fd491f11a24ba86072ef05211",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-900.woff2"
+  },
+  {
+    "revision": "33dce6ef52242c5a361d72aeef301938",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-regular.woff"
+  },
+  {
+    "revision": "ba4ba4d488d22a2d51406cffc05cb4ba",
+    "url": "/fonts/m-plus-rounded/m-plus-rounded-1c-v10-latin_cyrillic-regular.woff2"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "75344228492664fc27c5da60cbac4459",
+    "url": "/img/cover.png"
+  },
+  {
+    "revision": "740a759e7cf7d5991be3edff39ea174b",
+    "url": "/img/icon.png"
+  },
+  {
+    "revision": "ff37814064381e1de78a9328ff626c99",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "b0a67b168562f50ee8886523aa5a7cc7",
+    "url": "/img/splashes/chat.png"
+  },
+  {
+    "revision": "8a5b2b2c89deb3d40e73cf38128e8612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "772c7c69fed5a9f4b877",
+    "url": "/js/app.8449fc02.js"
+  },
+  {
+    "revision": "0fc6657250b99b446f28",
+    "url": "/js/chunk-vendors.d1b156b8.js"
+  },
+  {
+    "revision": "9a09fa0d3071a1261a43",
+    "url": "/js/game.4db9e7b8.js"
+  },
+  {
+    "revision": "99ed0a78a42578f8d3d8",
+    "url": "/js/gameover.a76ba046.js"
+  },
+  {
+    "revision": "ab5519c24b2094a7ef2f9329f9647af3",
+    "url": "/js/gen.worker.e6d09bf8.worker.js"
+  },
+  {
+    "revision": "31ad7c8d90c81cdfb2e9",
+    "url": "/js/nextlevel.468cf556.js"
+  },
+  {
+    "revision": "7ca3d58b1c0de025d2403ed6a77c39fe",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "50d9bacb8e417c2b4ab55193ad3a408a",
+    "url": "/manifest.json?1"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "169a3a76c7f8ae0911b6ea9fe098d000",
+    "url": "/sfx/correct_word.wav"
+  },
+  {
+    "revision": "a0f18bb784668c1a14177bf9c7339b9e",
+    "url": "/sfx/countdown.wav"
+  },
+  {
+    "revision": "eb24d1a7d9437064a43891ef95a4fb74",
+    "url": "/sfx/countdown_over.wav"
+  },
+  {
+    "revision": "817b3d8468667e4c37e97917323759e3",
+    "url": "/sfx/game_over.wav"
+  },
+  {
+    "revision": "1786088b92640569b4abf0cb9f6e9b81",
+    "url": "/sfx/goal_reached.wav"
+  },
+  {
+    "revision": "371ac0594b0c4443df4369e432cb9926",
+    "url": "/sfx/level_complete.wav"
+  },
+  {
+    "revision": "9b21a65219ca08230d6976be469baea4",
+    "url": "/sfx/time_almost_over.wav"
+  },
+  {
+    "revision": "b9daf107a01f32d3b0820cd21085e5bd",
+    "url": "/words/ru/all.txt"
+  },
+  {
+    "revision": "a86f4bad754a48a1d06004f1aab86459",
+    "url": "/words/ru/letters.txt"
+  },
+  {
+    "revision": "a9f5f19fe05ea88d62ea7611e2f242a4",
+    "url": "/words/ru/source.txt"
+  },
+  {
+    "revision": "ead25ea11d3e4a77104378cc442709c8",
+    "url": "/words/ru/w4candidates.txt"
+  },
+  {
+    "revision": "611016742e2038459694dcfa26c0ae57",
+    "url": "/words/ru/w5candidates.txt"
+  },
+  {
+    "revision": "a8a3eb2901ff9d0b8a9d5bedca9a556d",
+    "url": "/words/ru/w6candidates.txt"
+  },
+  {
+    "revision": "7645e013b0564127a890c9d462c61876",
+    "url": "/words/ru/w7candidates.txt"
+  }
+]);
